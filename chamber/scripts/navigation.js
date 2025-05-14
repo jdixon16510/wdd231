d2795c04342d56e1b3215b7badc8d5865ed79f7e
Joseph Dixon <jdixon@horncadcenter.com>
2025-05-14 15:20:22 -0600

    updated nave section

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
    const menuToggle = document.getElementById("menu-toggle");
-   const navLinks = document.querySelector(".nav-links");
+   const navLinks = document.querySelector("nav");
  
    menuToggle.addEventListener("click", () => {
      navLinks.classList.toggle("show");
